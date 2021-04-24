3e174ec96a5d0c645cbf1876f59148d7b72e9d42
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-04-24 23:29:56 +0530

    #jobs descrtion added and work samples updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import HomePage from './Frontend/containers/HomePage/HomePage';
+import ScrollToTop from './Frontend/components/ScrollToTop';
 
 function App() {
 
@@ -12,9 +13,9 @@ function App() {
   }, []);
 
   return (
-    <>
+    <ScrollToTop>
       <HomePage />
-    </>
+    </ScrollToTop>
   );
 }
 
